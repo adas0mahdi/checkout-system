36c7f199abe624a54099bae03972b7e4b38e570d
adas0mahdi <adas0mahdi@gmail.com>
2024-08-24 06:47:11 +0800

    Initial commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $productSuffix = ['T-shirt', 'Hat', 'Pants', 'Choose', 'Glass'];
+        $name = $this->faker->company() . '' . Arr::random($productSuffix);
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => $this->faker->realText(200),
+            'price' => $this->faker->numberBetween(1000, 100000),
+        ];
+    }
+}
