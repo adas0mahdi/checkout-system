ff061bdf55e0009a8e4c784e73a54de57845cc77
adas0mahdi <adas0mahdi@gmail.com>
2024-08-24 07:44:40 +0800

    add vue and tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="bg-green-500">
+        ADAS MAHDI  🤘 
+    </h1>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
